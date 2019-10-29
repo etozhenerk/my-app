2d72d33236ffd83ce6ee177eab678acc2bc1f606
etozhenerk <etozhenerk@yandex.ru>
2019-10-29 17:07:37 +0300

    add build

---
+++ build/precache-manifest.f4955bb836f87e1d75ac89fe5187933c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d553607a49f18278dd10f9d32f4c1ca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "186b505ba7c0d5e74c87",
+    "url": "/static/css/main.fcb48c17.chunk.css"
+  },
+  {
+    "revision": "8ebcb2e5d85cf535ee4c",
+    "url": "/static/js/2.44402982.chunk.js"
+  },
+  {
+    "revision": "186b505ba7c0d5e74c87",
+    "url": "/static/js/main.434494e3.chunk.js"
+  },
+  {
+    "revision": "1f3c89d7d8e9d2ccca39",
+    "url": "/static/js/runtime-main.e89362ac.js"
+  },
+  {
+    "revision": "3b8ad90a7a5366149b0595e667396d65",
+    "url": "/static/media/error.3b8ad90a.png"
+  },
+  {
+    "revision": "9e8a2b3937952b3de7aca04cb0d74020",
+    "url": "/static/media/logo.9e8a2b39.svg"
+  }
+]);
